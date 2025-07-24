36345386c02716d89cc4e370ce3ff14f146e77f4
Michael Klemm <michael@dontknow.de>
2025-07-24 16:17:37 +0000

    Update on Overleaf.

---
+++ code/tgt_loop.f90
@@ -0,0 +1,12 @@
+subroutine saxpy(x, y, a, n)
+    use iso_fortran_env, only: rt=>real32
+    implicit none
+    real(kind=rt), dimension(n) :: x
+    real(kind=rt), dimension(n) :: y
+    real(kind=rt) :: a
+    integer :: i
+    !$omp target teams loop map(to:x)
+    do i = 1, n
+        y(i) = a * x(i) + y(i) 
+    end do
+end subroutine
