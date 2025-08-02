50f69bf0682609d3240b4db8493785dec8b85cfd
Michael Klemm <michael@dontknow.de>
2025-08-02 13:57:13 +0200

    Use less lines for Listing 1

--- code/tgt_loop.f90
+++ code/tgt_loop.f90
@@ -1,11 +1,9 @@
 subroutine saxpy(x, y, a, n)
     use iso_fortran_env, only: rt=>real32
     implicit none
-    real(kind=rt), dimension(n) :: x
-    real(kind=rt), dimension(n) :: y
+    real(kind=rt), dimension(n) :: x, y
     real(kind=rt) :: a
-    integer :: n
-    integer :: i
+    integer :: n, i
     !$omp target teams loop map(to:x)|\label{ln:SaxpyMapClause}|
     do i = 1, n
         y(i) = a * x(i) + y(i) 
