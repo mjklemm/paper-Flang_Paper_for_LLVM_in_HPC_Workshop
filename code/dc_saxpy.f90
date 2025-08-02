b90c6e454f3a82dd38bcae3e72c3652b68d2bf47
Michael Klemm <michael@dontknow.de>
2025-08-02 14:37:21 +0200

    Be denser

--- code/dc_saxpy.f90
+++ code/dc_saxpy.f90
@@ -1,13 +1,10 @@
 subroutine saxpy2d(x, y, a, n, m)
-   use iso_fortran_env, only: rt=>real32
-   implicit none
-   real(kind=rt), dimension(n,m) :: x
-   real(kind=rt), dimension(n,m) :: y
-   real(kind=rt) :: a
-   integer :: n, m
-   integer :: i, j
-
-   do concurrent(i=1:n, j=1:m)
-       y(i,j) = a * x(i,j) + y(i,j)
-   end do
+    use iso_fortran_env, only: rt=>real32
+    implicit none
+    real(kind=rt), dimension(n,m) :: x, y
+    real(kind=rt) :: a
+    integer :: n, m, i, j
+    do concurrent(i=1:n, j=1:m)
+        y(i,j) = a * x(i,j) + y(i,j)
+    end do
 end subroutine saxpy2d
