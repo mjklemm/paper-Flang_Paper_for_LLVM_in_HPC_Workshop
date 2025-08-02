da240ed12549cfc99c3f39d22af0055fb4d375f6
Michael Klemm <michael@dontknow.de>
2025-08-02 15:00:56 +0200

    Format code

--- code/loop_dir_teams_2.f90
+++ code/loop_dir_teams_2.f90
@@ -1,7 +1,7 @@
 !$omp teams loop collapse(2)
 do i = 1, n
-  do j = 1, m
-    val(i,j) = i + j
-    call foo()
-  end do
+    do j = 1, m
+        val(i,j) = i + j
+        call foo()
+    end do
 end do
