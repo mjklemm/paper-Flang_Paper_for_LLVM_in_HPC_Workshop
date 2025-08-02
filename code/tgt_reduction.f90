4c322d242b6d84ecdd02b626ed92e9699c11c015
Michael Klemm <michael@dontknow.de>
2025-08-02 14:24:27 +0200

    Fix reduction example

--- code/tgt_reduction.f90
+++ code/tgt_reduction.f90
@@ -1,10 +1,13 @@
 program reduction
-      real(8):: s, a(10000)
-      s=0.0d0
+    use iso_fortran_env, only: rt-=>real64
+    implicit none
+    real(kind=rt) :: s, a(10000)
+    integer :: i
+    s = 0.0_rt
 !$omp target teams distribute parallel do reduction(+:s)
-      do i=1, 10000
-         s=s+a(i)
-      enddo
+    do i = 1, 10000
+        s = s + a(i)
+    enddo
 !$omp end target teams distribute parallel do
-      print *, s
+    print *, s
 end program
