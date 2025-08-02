0fa511333e320ac92f554a656f8523c08c51d297
Michael Klemm <michael@dontknow.de>
2025-08-02 15:05:02 +0200

    Format code

--- code/arrays.f90
+++ code/arrays.f90
@@ -1,16 +1,18 @@
 program main
-   real, allocatable :: x(:), a(:), b(:)
-   real :: y(256), c(256), d(256)
-   ! allocate dynamic arrays x,a,b
-   allocate(x(256), a(256), b(256))
-   ! OpenMP kernel with dynamic arrays
-   !$omp target teams distribute parallel do
-   do i = 1, 255
-      x(i) = a(i) + b(i)
-   end do
-   ! OpenMP kernel with static arrays 
-   !$omp target teams distribute parallel do
-   do i = 1, 255
-      y(i) = c(i) + d(i)
-   end do
+    implicit none
+    real, allocatable :: x(:), a(:), b(:)
+    real :: y(256), c(256), d(256)
+    integer :: i
+    ! allocate dynamic arrays x, a, b
+    allocate(x(256), a(256), b(256))
+    ! OpenMP kernel with dynamic arrays
+!$omp target teams distribute parallel do
+    do i = 1, 256
+        x(i) = a(i) + b(i)
+    end do
+    ! OpenMP kernel with static arrays 
+!$omp target teams distribute parallel do
+    do i = 1, 256
+        y(i) = c(i) + d(i)
+    end do
 end program
